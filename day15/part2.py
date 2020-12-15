433cd2ac3792532f3aab1fb948f16351bfe48dfb
Bram <bramdenouden96@hotmail.com>
2020-12-15 11:31:22 +0100

    interface added

--- day15/part2.py
+++ day15/part2.py
@@ -1,4 +1,5 @@
 import sys
+from time import time
 
 with open(sys.path[0] + '/input.txt') as f:
     for line in f:
@@ -8,6 +9,7 @@ turn = len(numbers)  # index of the turn (actual turn is 1 larger)
 lastOccurence = {num: i for i, num in enumerate(numbers)}
 
 numbers = numbers[-2:]
+startTime = time()
 while True:
     lastSpoken = numbers[-1]
     lastOccured = lastOccurence.get(lastSpoken, None)
@@ -24,12 +26,19 @@ while True:
     lastOccurence[numbers[-2]] = turn - 1
     numbers = numbers[-2:]  # decrease buffer
 
-    if turn % 10000 == 0:
-        print(turn)
+    if (turn + 1) % 10000 == 0:
+        percentComplete = 100*turn / (30000000-1)
+        estTotTime = (time()-startTime)/(percentComplete/100)
+        estDeltaTime = estTotTime - (time()-startTime)
+        sys.stdout.write(
+            "\rProgress: %d %%,\t Estimated time left: %d sec" %
+            (percentComplete, estDeltaTime)
+        )
 
     if turn >= 30000000 - 1:
         print(f"\nSolution: {numbers[-1]}")
-        exit('Reached last turn!')
+        exit(
+            f'Reached last turn! total execution time was {time()-startTime} seconds')
 
     turn += 1
 
