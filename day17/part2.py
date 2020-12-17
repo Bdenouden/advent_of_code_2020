bf1fb638a149237986d504cf118eae01259e68f6
bram <bramdenouden96@hotmail.com>
2020-12-17 22:08:45 +0100

    day17 p2 finished

--- day17/part2.py
+++ day17/part2.py
@@ -1,18 +1,20 @@
 import sys
 from pprint import pprint
 
-zLayers = {0: list()}
+space = {0: {0: list()}}
+
 
 with open(sys.path[0] + '/input.txt') as f:
     for line in f:
-        zLayers[0].append([c for c in line.strip()])
+        space[0][0].append([c for c in line.strip()])
 
 
 def checkXY(Cx, Cy,  plane):
     """
     arguments: cube x, y and z.
     checks how many active neighbours there are in this XY plane
-    """
+    """   
+   
     activeCount = 0
     yMin = max(Cy-1, 0)
     yMax = min(Cy+2, len(plane))
@@ -28,82 +30,94 @@ def checkXY(Cx, Cy,  plane):
     return activeCount
 
 
-def checkNeigbors(Cx, Cy, Cz):
+def checkNeigbors(Cx, Cy, Cz, Cw):
     activeCount = 0
-    zLen = len(zLayers[0])
-
-    zMin = max(Cz-1, min(zLayers))
-    zMax = min(Cz+2, max(zLayers)+1)
-
-    # check layer above and below this one
-    for i in range(zMin, zMax):
-        activeCount += checkXY(Cx, Cy, zLayers.get(i, [['.']*zLen]*zLen))
+    yLen = len(space[0][0])
+ 
+    wMin = max(Cw-1, min(space))
+    wMax = min(Cw+2, max(space)+1)
+    
+    # iterate over w space
+    for w in range (wMin, wMax):
+        
+        # check layer above and below this one
+        zMin = max(Cz-1, min(space[w]))
+        zMax = min(Cz+2, max(space[w])+1)
+        for i in range(zMin, zMax):
+            activeCount += checkXY(Cx, Cy, space[w].get(i, [['.']*yLen]*yLen))
 
     # return active neighbors and correct for own value
-    return activeCount - (zLayers[Cz][Cy][Cx] == '#')
+    return activeCount - (space[Cw][Cz][Cy][Cx] == '#')
 
 
-def enforeRules(nCx, nCy, nCz, activeNb, nextCubeConfig):
-    isActive = zLayers[nCz][nCy][nCx] == '#'
+def enforeRules(nCx, nCy, nCz, nCw, activeNb, nextCubeConfig):
+    isActive = space[nCw][nCz][nCy][nCx] == '#'
 
     if(isActive and 2 <= activeNb <= 3):
-        nextCubeConfig[nCz][nCy][nCx] = '#'
+        nextCubeConfig[nCw][nCz][nCy][nCx] = '#'
     elif(not isActive and activeNb == 3):
-        nextCubeConfig[nCz][nCy][nCx] = '#'
+        nextCubeConfig[nCw][nCz][nCy][nCx] = '#'
     else:
-        nextCubeConfig[nCz][nCy][nCx] = '.'
+        nextCubeConfig[nCw][nCz][nCy][nCx] = '.'
     return nextCubeConfig
+                
 
 
 def expandSpace(space):
-    zLen = len(space[0]) + 2
-    for w in space:
-        for z in w:
+    yLen = len(space[0][0]) + 2       
+    for w in space.copy():
+        for z in space[w].copy():
             # append start and end .
             for y in range(len(space[w][z])):
                 space[w][z][y].append('.')
                 space[w][z][y].insert(0, '.')
 
             # append new top, bottom row
-            space[w][z].insert(0, ['.']*zLen)
-            space[w][z].append(['.']*zLen)
+            space[w][z].insert(0, ['.']*yLen)
+            space[w][z].append(['.']*yLen)
 
-    # append new layers
-    space[min(space)-1] = [['.']*zLen]*zLen
-    space[max(space)+1] = [['.']*zLen]*zLen
+        # append new layers
+        space[w][min(space[w])-1] =  [['.']*yLen]*yLen
+        space[w][max(space[w])+1] =  [['.']*yLen]*yLen
+    
+    # set new w spaces
+    for w in [min(space)-1, max(space)+1]:
+        space[w] = {}
+        for z in space[0]:
+            space[w][z] = [['.']*yLen]*yLen
+
     return space
 
 
 cycle = 0
-
-
-# its an infinite space so only the relevant parts are shown
-# https://www.reddit.com/r/adventofcode/comments/ker0wi/2020_day_17_part_1_sample_input_wrong/
-
 while cycle < 6:
     # Cycle through each element on the Z layers and the one above and below
 
-    zLayers = expandSpace(zLayers)
-    nextSpace = {key: [[x for x in y] for y in val] for key, val in zLayers.items()}
+    space = expandSpace(space)    
+    nextSpace = {wkey: {zkey: [[x for x in y] for y in val] for zkey, val in wval.items()} for wkey, wval in space.items()}
 
-    for z in range(min(zLayers), max(zLayers)+1):
-        xyPlane = zLayers[z]
+    for w in range(min(space), max(space)+1):
+        zField = space[w]
+        
+        for z in range(min(zField), max(zField)+1):
+            xyPlane = zField[z]
 
-    # this iteration should expand each xy plane as well
-        for y in range(len(xyPlane)):
-            for x in range(len(xyPlane[0])):
-                neighbors = checkNeigbors(x, y, z)
-                nextSpace = enforeRules(x, y, z, neighbors, nextSpace)
+            # this iteration should expand each xy plane as well
+            for y in range(len(xyPlane)):
+                for x in range(len(xyPlane[0])):
+                    neighbors = checkNeigbors(x, y, z, w)
+                    nextSpace = enforeRules(x, y, z,w, neighbors, nextSpace)
 
-    zLayers = {key: [[x for x in y] for y in val] for key, val in nextSpace.items()}
+    space = {wkey: {zkey: [[x for x in y] for y in val] for zkey, val in wval.items()} for wkey, wval in nextSpace.items()}
     cycle += 1
-
+        
 counter = 0
-for z in sorted(zLayers):
-    for y in zLayers[z]:
-        for x in y:
-            if x == '#' :
-                counter += 1
+for w in sorted(space):
+    for z in sorted(space[w]):
+        for y in space[w][z]:
+            for x in y:
+                if x == '#':
+                    counter += 1
 
 print(f"A total of {counter} active nodes was found")
 
